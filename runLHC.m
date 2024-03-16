794c118572cdb6ce7874eb2ed4885bd66d3c8161
Aidan Gettemy <90980521+Aidan-Gettemy@users.noreply.github.com>
2024-03-16 16:23:25 -0500

    Update runLHC.m

--- runLHC.m
+++ runLHC.m
@@ -40,23 +40,17 @@ trans = 30;
 
 % Run the Simulations
 
+% SET WHERE TO START THE TESTS
+restart = 1;
+
 % As we go, write down the result folder of each completed test to a file
 % that tracks the progress of the simulation
-for i = 1:numel(M(:,1))
-    % If we have 100 points, its time to take a break
-    if i == 230
-        ttl = "Process some of the results";
-        shshs = "*****************************************************************";
-        for h = 1:5
-            disp(shshs)
-        end
-        disp(ttl)
-        break
-    end
+for i = restart:numel(M(:,1))
+    
     % Pick up where we left off:
     try
         data = gather_up(StatusFileID); % see if we already have the status file
-        i = numel(data)+ 1;
+        i = numel(data)+ restart;
     catch
         i = i;
     end
@@ -110,16 +104,16 @@ for i = 1:numel(M(:,1))
 
 
     % As we go, write down the result folder of each completed test to status file:
-    if i>1
-        test_cell = cell(1,i);
+    if i==restart
+        test_cell = cell(1,i-restart+1);
+        test_cell{1,1} = ExperimentID+"/"+TestID;
+    else
+        test_cell = cell(1,i-restart+1);
         data = gather_up(StatusFileID);
         for j = 1:numel(data)
             test_cell{1,j} = data{1,j};
         end
-        test_cell{1,i} = ExperimentID+"/"+TestID;
-    else
-        test_cell = cell(1,i);
-        test_cell{1,1} = ExperimentID+"/"+TestID;
+        test_cell{1,j+1} = ExperimentID+"/"+TestID;
     end
     fileID = fopen(StatusFileID,'w');
     fprintf(fileID,'%s\n',test_cell{:});
@@ -128,18 +122,109 @@ for i = 1:numel(M(:,1))
     display = ['Currently finished with test ',num2str(i)];
     disp(display)
 end
+%% This will allow us to remake the statusfile
+% Link the helper function
+addpath funcs/
+
+% Make sure that we have the right test points to run
+M = readmatrix('LHC_220.txt');
+
+% ExperimentID: Where results will be held.
+ExperimentID = "Data/LHC_220";
+
+% StatusFileID: This stores which tests have already run
+StatusFileID = "LHC_220_Status.txt";
+
+% This shows what each entry in the input vector holds
+
+% Go ahead and make all the prep-folders and files
+status = mkdir(ExperimentID);
+
+% We must set the test duration:
+test_dur = 160;
+
+% blade pitch control (0=off, 1=on)
+bld_fix = 1;
+
+% we average last seconds
+trans = 40;
+
+restart = 1;
+
+for i = restart:numel(M(:,1))
+    % Pick up where we left off:
+    try
+        data = gather_up(StatusFileID); % see if we already have the status file
+        i = numel(data)+restart;
+    catch
+        i = i;
+    end
+
+    % Set the vector input
+    vector = [0,11.4,0,1225,...
+            0,0,0,0,0,0,...
+                0,0,0,0,0,0,...
+                    0,0,0,0,0,0];
+    run_point = M(i,:);
+
+    % translate each input into the relevant range (inputs are between 0 and 1)
+    vector(1,1) = 15*run_point(1,1);% wind direction
+    vector(1,2) = 3+17*run_point(1,2);% wind speed
+    vector(1,3) = 0;% start the blade pitch at 0;
+    vector(1,4) = 1225*.95 + 1225*(0.1)*run_point(1,3); %air density
+    vector(1,5:22) = run_point(1,4:21);
+    
+    % make erosion realistic
+
+    for b = 1:3
+        %region1-2
+        vector(1,5+6*(b-1)) = .25*vector(1,5+6*(b-1));
+        vector(1,6+6*(b-1)) = .25*vector(1,6+6*(b-1));
+        %region3-4
+        vector(1,7+6*(b-1)) = .5*vector(1,7+6*(b-1));
+        vector(1,8+6*(b-1)) = .5*vector(1,8+6*(b-1));
+        %region5-6
+        vector(1,9+6*(b-1)) = vector(1,9+6*(b-1));
+        vector(1,10+6*(b-1)) = vector(1,10+6*(b-1));
+    end
+
+    % Set up the test
+    [status,TestID] = set_up(vector, i, "Template_NREL5MW",...
+    "Simulate_NREL5MW",test_dur,bld_fix);
+    
+    
+    % As we go, write down the result folder of each completed test to status file:
+    if i==restart
+        test_cell = cell(1,i-restart+1);
+        test_cell{1,1} = ExperimentID+"/"+TestID;
+    else
+        test_cell = cell(1,i-restart+1);
+        data = gather_up(StatusFileID);
+        for j = 1:numel(data)
+            test_cell{1,j} = data{1,j};
+        end
+        test_cell{1,j+1} = ExperimentID+"/"+TestID;
+    end
+    fileID = fopen(StatusFileID,'w');
+    fprintf(fileID,'%s\n',test_cell{:});
+    fclose(fileID);
+    % Indicate the progress:
+    display = ['Currently finished with test ',num2str(i)];
+    disp(display)
+end
+
 %% Now make the summary tables etc.
-trans0 = 30;% We will set the start of non-transitory behaviour to average over
+trans0 = 40;% We will set the start of non-transitory behaviour to average over
 data = gather_up(StatusFileID);
-for i = 1:numel(data)
+for i = 1:128%numel(data)
     trans = trans0;%reset on each iteration
     disp(data{i})
     line = split(data{i},"/");
     TestID = line{3};
     
     % Make big table
-    test_out = ExperimentID + "/" + TestID + "/" + TestID + ".out";
-    [test1outs,stat1] = create_mat_files(test_out);
+    % test_out = ExperimentID + "/" + TestID + "/" + TestID + ".out";
+    % [test1outs,stat1] = create_mat_files(test_out);
     
     % Now make the summary files
     SumID = ExperimentID + "/" + TestID + "/" +"Sensor_Data";
