215829dd4781fd81a174bfc9f2aee727f6855a3a
Aidan Gettemy <90980521+Aidan-Gettemy@users.noreply.github.com>
2024-03-16 16:32:10 -0500

    Update and rename BigExpCollator.m to LargeExpDataCollecter.m

--- LargeExpDataCollecter.m
+++ LargeExpDataCollecter.m
@@ -17,8 +17,8 @@ STDmat = zeros(numel(data),numel(names));
 MEANmat = zeros(numel(data),numel(names));
 
 % These next two variables explore some additional features which might be helpful
-%DOMFREQmat = zeros(numel(data),numel(names));
-%SUBFREQmat = zeros(numel(data),numel(names));
+DOMFREQmat = zeros(numel(data),numel(names));
+SUBFREQmat = zeros(numel(data),numel(names));
 
 for i = 1:numel(data)
     tableID = data{1,i}+"/Sensor_Data/SensorData_Mean_Std.txt";
@@ -26,21 +26,24 @@ for i = 1:numel(data)
     for j = 1:numel(varnames)
         STDmat(i,j) = table_mstd{j,2};
         MEANmat(i,j) = table_mstd{j,1};
-        %DOMFREQmat(i,j) = table_mstd{j,3};
-        %SUBFREQmat(i,j) = table_mstd{j,4};
+        DOMFREQmat(i,j) = table_mstd{j,3};
+        SUBFREQmat(i,j) = table_mstd{j,4};
     end
 end
 
 StdTab = array2table(STDmat,"VariableNames",varnames);
 MeanTab = array2table(MEANmat,"VariableNames",varnames);
 
-%DFTab = array2table(DOMFREQmat,"VariableNames",varnames);
-%SFTab = array2table(SUBFREQmat,"VariableNames",varnames);
+DFTab = array2table(DOMFREQmat,"VariableNames",varnames);
+SFTab = array2table(SUBFREQmat,"VariableNames",varnames);
 
 meantabID = "";
-stdtableID = "";
-
+stdtabID = "";
+f1tabID = "";
+f2tableID = "";
 writetable(MeanTab,meantabID);
-writetable(StdTab,stdtableID);
+writetable(StdTab,stdtabID);
+writetable(DFTab,f1tabID);
+writetable(SFTab,f2tabID);
 
 % consider using the join table GUI to combine multiple tables at once...
