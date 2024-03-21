f4ed4949421fc765f4345089d656a6f299cf4626
Aidan Gettemy <90980521+Aidan-Gettemy@users.noreply.github.com>
2024-03-21 11:22:46 -0500

    Update save_genpwr_ts.m

--- funcs/save_genpwr_ts.m
+++ funcs/save_genpwr_ts.m
@@ -1,1 +1,10 @@
+function status = save_genpwr_ts(tablewithgnpwrID)
+    data = readtable(tablewithgnpwrID);
+    y = data.Gen_Pwr.Variables;
+    x = data.Time.Variables;
+    
+    plot(x,y)
+    title
+    
 
+end
