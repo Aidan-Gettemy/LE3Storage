133ec8ec7dd9c51dfbef8ac5ed963dae90fb9d69
Aidan Gettemy <90980521+Aidan-Gettemy@users.noreply.github.com>
2024-03-21 11:47:05 -0500

    Update remake_head.m

--- funcs/remake_head.m
+++ funcs/remake_head.m
@@ -1,3 +1,19 @@
-function header = remake_head(input_vector,template_file,test_num,test_dur,bld_fix)
+function header = remake_head(input_vector,iter)
+ % make the file name 
+    name = "";
+    for i = 1:4
+        var = num2str(input_vector(1,i));
+        name = name + "_" + var;
+    end
+    
+    y = sum(input_vector(1,5:end));
 
- header = make_readme(input_vector,template_file,test_num,test_dur,bld_fix);
+    if y > 8
+        suf = "_severe_";
+    elseif y > 4
+        suf = "_moderate_";
+    else
+        suf = "_mild_";
+    end
+    header = "Test" + num2str(iter) + name + suf;
+end
