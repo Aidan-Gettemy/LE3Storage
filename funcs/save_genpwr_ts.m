2a2e6a2665bd98bb170c38d92e4f65eb56b93f9c
Aidan Gettemy <90980521+Aidan-Gettemy@users.noreply.github.com>
2024-03-21 15:51:28 -0500

    Add files via upload

--- funcs/save_genpwr_ts.m
+++ funcs/save_genpwr_ts.m
@@ -1,16 +1,15 @@
-function status = save_genpwr_ts(tablewithgnpwrID, testnum,testfolder)
+function status = save_genpwr_ts(tablewithgnpwrID, testnum, testfolder)
     data = readtable(tablewithgnpwrID);
-    y = data.Gen_Pwr.Variables;
-    x = data.Time.Variables;
+    y = data.GenPwr;
+    x = data.Time;
     f = figure(Visible="off");
     plot(x,y)
-    titlename = 
     xlabel("Time (s)");
     ylabel("Generator Power (kW)");
     trl = "Generator Power vs Time for Test # " + num2str(testnum);
     title(trl);
     
-    prt = testfolder + "/" + "GenPwrvsTime.pdf";   
-    print(gcf,prt,"-dpdf")
-
+    prt = testfolder + "/" + "GenPwrvsTimeTest"+num2str(testnum)+".pdf";   
+    print(f,prt,"-dpdf")
+    status = "ok";
 end
--- funcs/savesmalltbl.m
+++ funcs/savesmalltbl.m
@@ -1,11 +1,17 @@
-function stat = savesmalltbl(tableID,vector)
+function stat = savesmalltbl(tableID,saveID,vector)
   datat = readtable(tableID);
   names = datat.Properties.VariableNames;
-  smallT = table();
+  savednames = names(vector);
+  x = datat(:,vector(1));
+  x = x.Variables;
+  M = zeros(numel(x),numel(vector));
   for i = 1:numel(vector)
-    x = datat(:,x_value);
+    x = datat(:,vector(i));
     x = x.Variables;
-    smallT.names(i) = x;
+    M(:,i) = x;
   end
-  
+  smallT = array2table(M);
+  smallT.Properties.VariableNames = savednames;
+  writetable(smallT,saveID)
+  stat = "okay";
 end
