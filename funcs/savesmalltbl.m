20679a57f6f436dbf682dce581d7c3df68d0fe10
Aidan Gettemy <90980521+Aidan-Gettemy@users.noreply.github.com>
2024-03-21 12:03:36 -0500

    Update savesmalltbl.m

--- funcs/savesmalltbl.m
+++ funcs/savesmalltbl.m
@@ -1,10 +1,11 @@
 function stat = savesmalltbl(tableID,vector)
   datat = readtable(tableID);
-  
+  names = datat.Properties.VariableNames;
+  smallT = table();
   for i = 1:numel(vector)
     x = datat(:,x_value);
     x = x.Variables;
-    
+    smallT.names(i) = x;
   end
-
+  
 end
