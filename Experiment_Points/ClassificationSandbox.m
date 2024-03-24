d7b4197340c7674a0189f38059ea93a150d0d8f5
Aidan Gettemy <90980521+Aidan-Gettemy@users.noreply.github.com>
2024-03-24 18:12:02 -0500

    Update ClassificationSandbox.m

--- Experiment_Points/ClassificationSandbox.m
+++ Experiment_Points/ClassificationSandbox.m
@@ -10,7 +10,60 @@ save('Tree_4Class_TrainedModel.mat','-struct','TreeEr4Class');
 save('NN_4Class_TrainedModel.mat','-struct','NN_ER4Class');
 
 %% We can load in the model
-load("RF_Opt_4Class_TrainedModel.mat")
+clc;close;clear;
+load("RF_Opt_4Class_TrainedModel.mat");
+predImp = ClassificationEnsemble.predictorImportance;
+y = array2table(ClassificationEnsemble.predictorImportance');
+y.Properties.RowNames = ClassificationEnsemble.ExpandedPredictorNames;
+ysorted = sortrows(y);
+
+labels = ysorted.Properties.RowNames(200:end);
+vals = ysorted(200:end,1).Variables;
+
+barh(labels,vals);
+title("Most Important Predictors")
+
+%% Play around with it:
+
+HowToPredict
+DataT = readtable("/Volumes/ARG2024/4Class_ExpDataTable.txt");
+
+DataT(:,616:634) = [];
+
+
+% Perform cross-validation
+partitionedModel = crossval(ClassificationEnsemble, 'KFold', 5);
+
+% Compute validation predictions
+[validationPredictions, validationScores] = kfoldPredict(partitionedModel);
+
+% Compute validation accuracy
+validationAccuracy = 1 - kfoldLoss(partitionedModel, 'LossFun', 'ClassifError');
+
+
+
+Out = ClassificationEnsemble.Y;
+% 
+% 
+% validation_fraction = .5;
+% ipointer=1:length(DataT(:,1).Variables);
+% cvpart=cvpartition(Out,'holdout',validation_fraction);
+% 
+% DataT_Test = DataT(test(cvpart),:);
+% Out_True = Out(test(cvpart),1);
+% 
+% [Out_Predict,score] = ClassificationEnsemble.predict(DataT_Test);
+%%
+figure
+plotconfusion(validationPredictions,Out)
+saveas(gcf,'4ClassConfusion_Clean.png')
+classNames = ClassificationEnsemble.ClassNames;
+rocObj = rocmetrics(Out,validationScores,classNames);
+rocObj.AUC
+figure
+plot(rocObj)
+saveas(gcf,'4ClassROC_Clean.png')
+
 %% Or, if we saved the train process as a function, call that here
 [trainedclassifier,acc] = Er4ClassRFAdaboost(readtable("4Class_ExpDataTable.txt"));
 %% Save figures
@@ -18,6 +71,7 @@ load("RF_Opt_4Class_TrainedModel.mat")
 % saveas(gcf,'4ClassConfusionMatrix.png')
 % title("Random Forest 94.5% Accuracy (5-fold)")
 % saveas(gcf,'4ClassROC.png')
-title("NN 84.6% Accuracy (5-fold)")
-saveas(gcf,'4ROCNN.png')
+% title("NN 84.6% Accuracy (5-fold)")
+% saveas(gcf,'4ROCNN.png')
+
 
