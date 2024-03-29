e8bd5cae3037fc120d7ba3b28c59bccf00f14946
Aidan Gettemy <90980521+Aidan-Gettemy@users.noreply.github.com>
2024-03-29 09:43:09 -0500

    Update CreateBladePitchExperiment.m

--- CreateBladePitchExperiment.m
+++ CreateBladePitchExperiment.m
@@ -1,16 +1,20 @@
 clc;close;clear;
 % Create the design points for the Blade Pitch vs Wind Speed Experiment
-% First we will try to run the tests with the blade pitch controller
-% See if for wind speeds above 11.4 m/s, will setting the blades at a
-% higher pitch prevent them from stalling.  In this experiment we will run
-% the clean turbine and the strongly eroded turbine.  If I lessen the
-% maximum erosion definition, I am going to see if I can try uniform
-% erosion
+% We have added some code to better set the initial rotor speed and blade pitch
+% The rotorspeed is set in the chg bld pitch setup function
+% We added an additional function that we call after plugging in the run point
+% Thus it will automatically read the wind speed and set pitch to the right level
 
-windspeeds = 3:26;
+windspeeds = [3:26,linspace(10.5,13.5,10)]
 
 erosion_profiles = {1/4*[1,1,2,2,4,4,1,1,2,2,4,4,1,1,2,2,4,4],...
-    0*[1,1,2,2,4,4,1,1,2,2,4,4,1,1,2,2,4,4];};
+    1/4*[1,1,2,2,4,4,1,1,2,2,4,4,1,1,2,2,4,4],...
+    0*[1,1,2,2,4,4,1,1,2,2,4,4,1,1,2,2,4,4],...
+    [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],...
+    2/3*[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],...
+    1/2*[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],...
+    1/6*[1,2,3,4,5,6,1,2,3,4,5,6,1,2,3,4,5,6],...
+    1/12*[1,2,3,4,5,6,1,2,3,4,5,6,1,2,3,4,5,6]};
 
 Design = zeros(1,22);
 noms = [0,11.4,0,1225];
@@ -19,17 +23,15 @@ iter = 1;
 for j = 1:numel(erosion_profiles)
     for i = 1:numel(windspeeds)
         ws = windspeeds(i);
-        bldptch = 0;
-        if j == 1
-            if ws>11.4
-                bldptch = 10;
-            end
-        end
         Design(iter,1:4) = noms;
-        Design(iter,[2,3]) = [ws,bldptch];
+        Design(iter,[2]) = [ws];
         Design(iter,5:end) = erosion_profiles{j};
         iter=iter+1;
     end
 end
 
 save("BldptchvsWindSpeed.txt","Design","-ascii")
+
+% Run the design points and then explore the resulting behaviour of the turbine
+% Did we fix all of the strange behaviour?
+% Check if any of the profiles are 'breaking' the model--will need to get multiple computers
