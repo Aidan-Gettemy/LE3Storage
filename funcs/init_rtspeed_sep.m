bb88fb03f915ee1c800ebe8cc50cf05411c2761e
Aidan Gettemy <90980521+Aidan-Gettemy@users.noreply.github.com>
2024-04-02 13:44:55 -0500

    Update init_rtspeed_sep.m

--- funcs/init_rtspeed_sep.m
+++ funcs/init_rtspeed_sep.m
@@ -1,4 +1,7 @@
 function stat = init_rtspeed_sep(input_vector, test_num, template_directory,...
     simulation_directory,test_dur,bld_fix)
     
+    readfile_ID = template_directory + "/modules/NRELOffshrBsline5MW_Onshore_ElastoDyn.dat";
+    writefile_ID = simulation_directory + "/modules/NRELOffshrBsline5MW_Onshore_ElastoDyn.dat";
+    
 end
