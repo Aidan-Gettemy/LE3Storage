d5a33925575cfe31894b3e04d36a5559ab8247a2
Aidan Gettemy <90980521+Aidan-Gettemy@users.noreply.github.com>
2024-04-02 09:51:35 -0500

    Update fix_init_bldptch.m

--- funcs/fix_init_bldptch.m
+++ funcs/fix_init_bldptch.m
@@ -1,4 +1,4 @@
 function pitch = fix_init_bldptch(windspeed)
-  a=[-0.001287,    0.1013,   -2.968,   39.65, -193];
+  a = [8.0206e-06	-0.0011	0.0585	-1.7942	32.7361	-355.5472	2.1315e+03	-5.4479e+03]*1000;
   pitch=max(polyval(a,windspeed),0);
 end
