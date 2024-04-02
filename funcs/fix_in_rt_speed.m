4c754871073810765735a1c5ba2b2216f0c04656
Aidan Gettemy <90980521+Aidan-Gettemy@users.noreply.github.com>
2024-04-02 10:30:20 -0500

    Create fix_in_rt_speed.m

---
+++ funcs/fix_in_rt_speed.m
@@ -0,0 +1,13 @@
+function pitch = fix_in_rt_speed(windspeed)
+  a = [-0.0011	0.0344	-0.4150	2.4330	-6.6729	13.7661];
+  b = [0.3788	-12.1417	129.9142	-452.2309];
+  c = [0.0012	-0.0234	12.1697];
+  if windspeed<10.25
+    rtspeed = polyval(a,windspeed);
+  elseif windspeed<11.4
+    rtspeed = polyval(b,windspeed);
+  else
+    rtspeed = polyval(c,windspeed);
+  end
+  
+end
