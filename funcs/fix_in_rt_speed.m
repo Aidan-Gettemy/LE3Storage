61707c01064cc7b61066e3399097ba1fcdc2a3d3
Aidan Gettemy <90980521+Aidan-Gettemy@users.noreply.github.com>
2024-04-02 14:01:21 -0500

    Update fix_in_rt_speed.m

--- funcs/fix_in_rt_speed.m
+++ funcs/fix_in_rt_speed.m
@@ -1,4 +1,4 @@
-function pitch = fix_in_rt_speed(windspeed)
+function rtspeed = fix_in_rt_speed(windspeed)
   a = [-0.0011	0.0344	-0.4150	2.4330	-6.6729	13.7661];
   b = [0.3788	-12.1417	129.9142	-452.2309];
   c = [0.0012	-0.0234	12.1697];
