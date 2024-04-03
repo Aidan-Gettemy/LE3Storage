92265a263f0f6f84ab4aa61e1ec0b02a52140e4e
Aidan Gettemy <90980521+Aidan-Gettemy@users.noreply.github.com>
2024-04-03 14:32:56 -0500

    Update CreateBladePitchExperiment.m

--- CreateBladePitchExperiment.m
+++ CreateBladePitchExperiment.m
@@ -5,37 +5,33 @@ clc;close;clear;
 % We added an additional function that we call after plugging in the run point
 % Thus it will automatically read the wind speed and set pitch to the right level
 
-windspeeds = [3:26,linspace(10,14,20),linspace(10,14,20)]
+windspeeds = linspace(11.4,13,20);
 
-erosion_profiles = {1/2*[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],...
-    0*[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]};
+erosion_profiles = {0*[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]};
+bladepitches = linspace(0,5.64,10);
 
 Design = zeros(1,22);
 noms = [0,11.4,0,1225];
 
 iter = 1;
-for g = 1:2
-    for j = 1:numel(erosion_profiles)
-        for i = 1:numel(windspeeds)
+for j = 1:numel(erosion_profiles)
+    for i = 1:numel(windspeeds)
+        for k = 1:numel(bladepitches)
             ws = windspeeds(i);
             Design(iter,1:4) = noms;
             Design(iter,[2]) = [ws];
-            if i > 24
-                Design(iter,[3]) = 1;
-            end
-            if i > 24
-                Design(iter,[3]) = 2;
-            end
+            Design(iter,3)=bladepitches(k);
             Design(iter,5:end) = erosion_profiles{j};
-            if g == 2
-                Design(iter,1) = 1;
-            end
             iter=iter+1;
         end
     end
 end
 
-save("BldptchvsWindSpeed3.txt","Design","-ascii")
+save("BldptchvsWindSpeed7.txt","Design","-ascii")
+
+% Run the design points and then explore the resulting behaviour of the turbine
+% Did we fix all of the strange behaviour?
+% Check if any of the profiles are 'breaking' the model--will need to get multiple computers
 
 % Run the design points and then explore the resulting behaviour of the turbine
 % Did we fix all of the strange behaviour?
