ea64c44b1554a2096c87f8345b38b2945b2e3b63
Aidan Gettemy <90980521+Aidan-Gettemy@users.noreply.github.com>
2024-04-03 14:33:39 -0500

    Update fix_in_rt_speed.m

--- funcs/fix_in_rt_speed.m
+++ funcs/fix_in_rt_speed.m
@@ -1,14 +1,5 @@
 function rtspeed = fix_in_rt_speed(windspeed)
-  a = [-0.0011	0.0344	-0.4150	2.4330	-6.6729	13.7661];
-  b = [0.3788	-12.1417	129.9142	-452.2309];
-  c = [0.0012	-0.0234	12.1697];
-  if windspeed<10.25
-    rtspeed = polyval(a,windspeed);
-  else 
-      if windspeed<11.4
-        rtspeed = polyval(b,windspeed);
-      else
-        rtspeed = polyval(c,windspeed);
-      end
-  end
+  y = readmatrix("rtpoints.txt");
+  x = 3:26;
+  rtspeed = min(spline(x,y,windspeed),12.1);
 end
