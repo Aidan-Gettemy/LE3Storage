f2bb98ad26da48d96d4a0635f39b482f709d7dcd
Aidan Gettemy <90980521+Aidan-Gettemy@users.noreply.github.com>
2024-04-23 15:20:13 -0500

    Update CombineDataPHYS_LEE.m

--- CombineDataPHYS_LEE.m
+++ CombineDataPHYS_LEE.m
@@ -3,7 +3,7 @@ addpath funcs/
 
 %% First, we will make one table of all of the output statistics
 
-StatusFileIDs = {""};
+StatusFileIDs = {'LEE_ERD1_25_Status.txt'};
 
 
 a = 1*ones(2);b = 2*ones(2);c = 3*ones(2);
@@ -23,11 +23,11 @@ names = load(nameID);
 names = names.Output_Names;
 
 varnames(1:3) = ["Wind_Direction","Wind_Speed","Air_Density"];
-suffixes = {"","_std","_f1","_f2"};
+suffixes = ["","_std","_f1","_f2"];
 iter = 4;
 for j = 1:4
     for i = 1:numel(names)
-        varnames(iter) = string(names{1,i}{1})+suffixes{j};
+        varnames(iter) = string(names{1,i}{1})+suffixes(j);
         iter = iter+1;
     end
 end
@@ -42,7 +42,7 @@ SUBFREQmat = zeros(numel(data),numel(names));
 for i = 1:numel(data)
     tableID = data{1,i}+"/Sensor_Data/SensorData_Mean_Std.txt";
     table_mstd = readtable(tableID,"ReadRowNames",true);
-    for j = 1:numel(varnames)
+    for j = 1:numel(varnames)/4
         STDmat(i,j) = table_mstd{j,2};
         MEANmat(i,j) = table_mstd{j,1};
         DOMFREQmat(i,j) = table_mstd{j,3};
@@ -55,7 +55,7 @@ ALLmat = cat(2,cat(2,cat(2,MEANmat,STDmat),DOMFREQmat),SUBFREQmat);
 
 
 %% Now, read in the input matrix
-M = readmatrix('LifeCycleErosionClasses.txt');
+M = readmatrix('LifeCycleErosionClasses_Test2.txt');
 
 wind_dir = M(:,1);
 wind_speed = M(:,2);
@@ -67,10 +67,12 @@ ALLmat = cat(2,cat(2,cat(2,wind_dir,wind_speed),air_dens),ALLmat);
 ALLTable = array2table(ALLmat,"VariableNames",varnames);
 
 % might have to remove the useless or unwanted predictor columns...
+ALLTable(:,[4:7,11,162:165,169,320:323,327,478:481,485])=[];
+
 
 ErosionRegions = M(:,4:21);
 
-InTable = array2table(ErosionRegions);
+OutTable = array2table(ErosionRegions);
 
 for i = 1:3
     for j = 1:6
@@ -85,10 +87,11 @@ OutTable.TurbineAgeClass = categorical(M(:,22));
 InTable = ALLTable;
 
 %% Save In and Out tables
-
+DataID = "_test2.txt";
+rng("default")
 % Testing Percentage = 25%
 p = 0.25;
-cvpart = cvpartition(OutTable(:,22),'holdout',p);
+cvpart = cvpartition(OutTable.TurbineAgeClass,'holdout',p);
 
 In_train = InTable(training(cvpart),:);
 Out_train = OutTable(training(cvpart),:);
@@ -97,10 +100,10 @@ In_test = InTable(test(cvpart),:);
 Out_test = OutTable(test(cvpart),:);
 
 
-writetable(InTable,"InTable_All.txt");
-writetable(OutTable,"OutTable_All.txt");
-writetable(In_train,"InTable_Train.txt");
-writetable(Out_train,"OutTable_Train.txt");
-writetable(In_test,"InTable_Test.txt");
-writetable(Out_test,"OutTable_Test.txt");
+writetable(InTable,"InTable_All"+DataID);
+writetable(OutTable,"OutTable_All"+DataID);
+writetable(In_train,"InTable_Train"+DataID);
+writetable(Out_train,"OutTable_Train"+DataID);
+writetable(In_test,"InTable_Test"+DataID);
+writetable(Out_test,"OutTable_Test"+DataID);
 
