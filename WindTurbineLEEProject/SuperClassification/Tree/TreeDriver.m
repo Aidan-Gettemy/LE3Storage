eae270d1fd76703d6959ac9813928f64a3055d91
Aidan Gettemy <90980521+Aidan-Gettemy@users.noreply.github.com>
2024-04-27 13:35:02 -0500

    Update TreeDriver.m

--- WindTurbineLEEProject/SuperClassification/Tree/TreeDriver.m
+++ WindTurbineLEEProject/SuperClassification/Tree/TreeDriver.m
@@ -1,89 +1,142 @@
 clc;close;clear;
 %% Designate Folder for plot results
 % Subfolder for plots
-plotID = "Evaluation";
-mkdir("Evaluation")
+plotID = "Evaluation/";
+mkdir("Evaluation/")
 
 %% Load in Data Inputs and Outputs 
 
-tableID = "../../4Class_ExpDataTable.txt";
+InTraintableID = "../../InTable_Train_1700.txt";
+InTesttableID = "../../InTable_Test_1700.txt";
 
-rawTable = readtable(tableID);
+OutTraintableID = "../../OutTable_Train_1700.txt";
+OutTesttableID = "../../OutTable_Test_1700.txt";
 
-In = rawTable(:,1:310);
-Out = categorical(rawTable(:,634).Variables);
+In_train = readtable(InTraintableID);
 
-% Testing Percentage = 20%
-p = 0.2;
-cvpart = cvpartition(Out,'holdout',p);
+In_test = readtable(InTesttableID);
 
-In_train = In(training(cvpart),:);
-Out_train = Out(training(cvpart),:);
+Out_train = readtable(OutTraintableID);
 
-In_test = In(test(cvpart),:);
-Out_test = Out(test(cvpart),:);
+Out_test = readtable(OutTesttableID);
 
-InNames = In.Properties.VariableNames;
-OutName = 'ErosionClass';
+% Just predict the class
+Out_train = categorical(Out_train(:,19).Variables);
+Out_test = categorical(Out_test(:,19).Variables);
 
-%% Set up the Model/Train the Model/Load Model
-tree = load(fileID);tree=tree.tree;
-%tree = fitctree(In_train,Out_train);
-%% Save the Model
-save("tree_class.mat",'tree');
+InNames = In_train.Properties.VariableNames;
+OutName = "ErosionAgeClass";
+
+% Now, assemble a matrix of the selected predictors for each output
+%predimpID = "../../FeatureSelection/.txt";
+%Importances=readmatrix(predimpID); (:,Importances(i,1:20))
+
+%% Set up the Model/Train the Model
+Mdl = fitctree(In_train,Out_train,...
+    'OptimizeHyperparameters','auto',...
+    'HyperparameterOptimizationOptions',struct('AcquisitionFunctionName',...
+    'expected-improvement-plus'));
+
+% Save the Model
+save("tree_class.mat",'Mdl');
 %% Evaluate the Model
 
+[Out_predict_train,scores_train] = Mdl.predict(In_train);
 
-[Out_predict_train,scores_train] = tree.predict(In_train);
+[Out_predict_test,scores_test] = Mdl.predict(In_test);
 
-[Out_predict_test,scores_test] = tree.predict(In_test);
+rocObj_train = rocmetrics(Out_train,scores_train,Mdl.ClassNames);
 
-rocObj_train = rocmetrics(Out_train,scores_train,tree.ClassNames);
-
-rocObj_test = rocmetrics(Out_test,scores_test,tree.ClassNames);
+rocObj_test = rocmetrics(Out_test,scores_test,Mdl.ClassNames);
 
 AUC_train = rocObj_train.AUC;
 AUC_test = rocObj_test.AUC;
 
 line1 = "Train: ";
 line2 = "Test: ";
-cats = categories(tree.ClassNames);
+cats = categories(Mdl.ClassNames);
 for i = 1:numel(cats)
     line1 = line1+"("+cats(i)+", "+num2str(AUC_train(i))+") ";
     line2 = line2+"("+cats(i)+", "+num2str(AUC_test(i))+") ";
 end
 disp(line1)
 disp(line2)
-% E_train = rmse(Out_predict_train, Out_train);
-% E_test = rmse(Out_predict_test, Out_test);
-% 
-% disp("RMSE Training: " + num2str(E_train));
-% disp("RMSE Testing: " + num2str(E_test));
-% 
-% Cc_train = corrcoef(Out_predict_train, Out_train);
-% Cc_test = corrcoef(Out_predict_test, Out_test);
-% 
-% disp("CC Training: " + num2str(Cc_train));
-% disp("CC Testing: " + num2str(Cc_test));
-file_cell = {line1,line2};
-file_name = plotID+"/summary_stats.txt";
-fileID = fopen(file_name,'w');
-fprintf(fileID,'%s\n',file_cell{:});
-fclose(fileID);
+% Save the AUC metrics: table with the average AUC training, AUC testing
+results = [mean(AUC_train),mean(AUC_test)];
+T = array2table(results,'VariableNames',{'avg AUC train','avg AUC test'});
+writetable(T,'ScoreTable.txt');
 %% Plot and Save as .png files
 
+% Training
+figure
+cm = confusionchart(Out_train,Out_predict_train);
+cm.Title = 'Tree: Training';
+cm.FontSize = 20;
+saveID = plotID+"CM_train.png";
+print('-dpng',saveID)
+
+% Grab the internal data
+n = numel(Mdl.ClassNames);
+x = get(rocObj_train.plot);
+turbocustom=turbo(n);
+colors = interp1(linspace(0, 24, n), turbocustom, linspace(0,24,n));
+f = figure;
+f.Position = [100 100 800 500];
+set(gca, 'ColorOrder', colors , 'NextPlot', 'replacechildren');
+hold on
+vals = linspace(0,24,17);
+for i = 1:numel(x)
+    lgd{i} = [num2str(vals(i))+" (AUC = "+num2str(AUC_train(i))+")"];
+    xdats = x(i).XData;
+    ydats = x(i).YData;
+    plot(xdats,ydats,'LineWidth',3)
+end
+grid on
+plot([0,1],[0,1],'LineStyle','--','LineWidth',4)
+lgd{i+1} = ["1:1"];
+xlabel("Flase Positive Rate")
+ylabel("True Positive Rate")
+title("Tree: ROC Training")
+fontsize(16,"points")
+legend(lgd,'Location','eastoutside')
+colormap(turbo(n))
+cb = colorbar;
+saveID = plotID+"ROC_train.png";
+print('-dpng',saveID)
+
+% Testing
 figure
 cm = confusionchart(Out_test,Out_predict_test);
-cm.Title = 'Erosion Class Detection (4-classes): Tree';
+cm.Title = 'Tree: Testing';
 cm.FontSize = 20;
+saveID = plotID+"CM_test.png";
+print('-dpng',saveID)
 
-figure
-rocObj_test.plot
-
-%% Try a Shapely Value test
-
-explainer1 = shapley(tree.tree,In_test);
-
-newExplainer = fit(explainer1,In_test(13,:));
-
-plot(newExplainer)
+% Grab the internal data
+n = numel(Mdl.ClassNames);
+x = get(rocObj_test.plot);
+turbocustom=turbo(n);
+colors = interp1(linspace(0, 24, n), turbocustom, linspace(0,24,n));
+f = figure;
+f.Position = [100 100 800 500];
+set(gca, 'ColorOrder', colors , 'NextPlot', 'replacechildren');
+hold on
+vals = linspace(0,24,17);
+for i = 1:numel(x)
+    lgd{i} = [num2str(vals(i))+" (AUC = "+num2str(AUC_test(i))+")"];
+    xdats = x(i).XData;
+    ydats = x(i).YData;
+    plot(xdats,ydats,'LineWidth',3)
+end
+grid on
+plot([0,1],[0,1],'LineStyle','--','LineWidth',4)
+lgd{i+1} = ["1:1"];
+xlabel("Flase Positive Rate")
+ylabel("True Positive Rate")
+title("Tree: ROC Testing")
+fontsize(16,"points")
+legend(lgd,'Location','eastoutside')
+colormap(turbo(n))
+cb = colorbar;
+saveID = plotID+"ROC_test.png";
+print('-dpng',saveID)
